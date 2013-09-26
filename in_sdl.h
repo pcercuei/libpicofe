25ad1fe93cca8eb3d9756476d46607c3b9b71a1c
Paul Cercueil <paul@crapouillou.net>
2013-09-26 08:16:55 -0300

    Let the platform code deliver the key mappings

--- in_sdl.h
+++ in_sdl.h
@@ -1,4 +1,17 @@
 struct in_default_bind;
 
-void in_sdl_init(const struct in_default_bind *defbinds,
+struct menu_keymap {
+	short key;
+	short pbtn;
+};
+
+struct in_sdl_pdata {
+	const struct in_default_bind *defbinds;
+	const struct menu_keymap *key_map;
+	size_t kmap_size;
+	const struct menu_keymap *joy_map;
+	size_t jmap_size;
+};
+
+int in_sdl_init(const struct in_sdl_pdata *pdata,
 		 void (*handler)(void *event));
--- linux/in_evdev.h
+++ linux/in_evdev.h
@@ -2,4 +2,15 @@
 struct in_default_bind;
 extern int in_evdev_allow_abs_only;
 
-void in_evdev_init(const struct in_default_bind *defbinds);
+struct menu_keymap {
+	short key;
+	short pbtn;
+};
+
+struct in_evdev_pdata {
+	const struct in_default_bind *defbinds;
+	const struct menu_keymap *key_map;
+	size_t kmap_size;
+};
+
+int in_evdev_init(const struct in_evdev_pdata *pdata);
